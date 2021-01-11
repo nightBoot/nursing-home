acf3c7d0c50dd5545b448223638d17b029ec3b53
white <15975904413@163.com>
2021-01-11 15:40:23 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h2>index</h2>
   </React.StrictMode>,
   document.getElementById('root')
 );
